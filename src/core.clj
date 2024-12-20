858956d929262d1c532737f6f1ea9d747f0326a4
Rafael Kosaka <rafael.y.kosaka@gmail.com>
2024-12-21 06:48:43 +0900

    fix :require

--- src/core.clj
+++ src/core.clj
@@ -1,7 +1,7 @@
 (ns core
-  (:require [io.github.rafa0805/clj-git-lib :as clj-git-lib]))
+  (:require [basic-math :as clj-git-lib]))
 
 
 (defn -main
-  [& args]
-  (println "Rusult : " (clj-git-lib.basic-math/sub 10 5)))
+  []
+  (println "Rusult : " (basic-math/sub 10 5)))
